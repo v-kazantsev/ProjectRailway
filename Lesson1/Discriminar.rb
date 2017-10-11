568cff413dd1712ca4547cbf1a30362330a1309a
v-kazantsev <vvs201@mail.ru>
2017-10-11 18:06:45 +0900

    Lesson1

---
+++ Lesson1/Discriminar.rb
@@ -0,0 +1,17 @@
+puts "Enter a"
+a = gets.chomp.to_i
+puts "Enter b"
+b = gets.chomp.to_i
+puts "Enter c"
+c = gets.chomp.to_i
+d = b**2 - 4*a*c
+if d < 0
+	puts "#{d}, no roots"
+elsif d == 0
+	puts "#{d}", (0-b)/(2*a)
+else
+	b = 0-b
+	x1 = (b + Math.sqrt(d))/(2*a)
+	x2 = (b - Math.sqrt(d))/(2*a)
+	puts x1, x2
+end
