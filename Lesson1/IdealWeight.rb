f503faf240fda64f41a97cf273876c04e6fd7e40
v-kazantsev <vvs201@mail.ru>
2017-10-11 21:46:09 +0900

    Lesson 1 Task 1 Revised

--- Lesson1/IdealWeight.rb
+++ Lesson1/IdealWeight.rb
@@ -1,10 +1,10 @@
 puts "What is your name?"
 name = gets.chomp
 puts "What is your height?"
-height = gets.chomp
-weight = height.to_i - 110
+height = gets.chomp.to_f
+weight = height - 110
 if weight < 0
- puts "#{name}, your weight is already optimal"
-	else 
-		puts "#{name}, your ideal weight is #{weight}"
-	end
+  puts "#{name}, your weight is already optimal"
+else 
+  puts "#{name}, your ideal weight is #{weight}"
+end
