6aed670af02a24b1cb16a06bc9fa6bc78987dd32
v-kazantsev <vvs201@mail.ru>
2017-10-13 23:21:54 +0900

    Lesson2 Task6 Revised

--- Lesson2/purchases.rb
+++ Lesson2/purchases.rb
@@ -1,16 +1,16 @@
-basket = Hash.new
+basket = {}
 total = 0
 loop do 
-puts "Введите товар"
-goods = gets.chomp.downcase
-break if goods == "стоп"
-puts "Введите цену за единицу"
-price = gets.chomp.to_f
-puts "Введите количество"
-qtty = gets.chomp.to_f
-basket[goods] = { price => qtty }
-total = total + price * qtty
-puts basket
-puts price * qtty
+  puts "Введите товар"
+  goods = gets.chomp.downcase
+  break if goods == "стоп"
+  puts "Введите цену за единицу"
+  price = gets.chomp.to_f
+  puts "Введите количество"
+  qtty = gets.chomp.to_f
+  basket[goods] = { price => qtty }
+  total = total + price * qtty
+  puts basket
+  puts price * qtty
 end
 puts total
