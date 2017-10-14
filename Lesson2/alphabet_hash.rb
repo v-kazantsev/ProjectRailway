0cc6219e0333dc26765f1e4b11d9c25fb82e834a
v-kazantsev <vvs201@mail.ru>
2017-10-14 09:17:16 +0900

    Lesson2 Task4 Revised

--- Lesson2/alphabet_hash.rb
+++ Lesson2/alphabet_hash.rb
@@ -1,10 +1,8 @@
 alphabet = Hash[("а".."я").to_a.zip((1..33).to_a)]
-vowels = ["а", "е", "ё", "и", "о", "у", "ы", "э", "ю", "я"]
+vowels = %(а е ё и о у ы э ю я)
 vowels_hash = {}
-for i in 0..9
-  alphabet.each do |letter, number|
-    vowels_hash[letter] = number if letter == vowels[i]
-  end
+alphabet.each do |letter, number|
+  vowels_hash[letter] = number if vowels.include?(letter)
 end
 puts vowels_hash
 
