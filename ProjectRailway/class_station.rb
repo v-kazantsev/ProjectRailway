84f5a49fdc4f2407bebd57167ae00ee8ae279e2e
v-kazantsev <vvs201@mail.ru>
2017-10-18 19:42:17 +0900

    Попытка №3

--- ProjectRailway/class_station.rb
+++ ProjectRailway/class_station.rb
@@ -1,30 +1,25 @@
 class Station
   require_relative "class_train"
 
-  attr_accessor :train_list
+  attr_accessor :trains 
 
   def initialize(name)       #создает станцию с заданным в аргументе именем
     @name = name
-    @train_list = [] 
-    @cargo_train = 0
-    @passenger_train = 0    
+    @trains = []    
   end
 
   def train_in(train)       #принимает поезд (объект класса Поезд)
-    self.train_list << train          
+    self.trains << train          
   end
 
   def train_out(train)     
-    train_list.delete(train)     
+    trains.delete(train)     
   end
 
   def train_type_list(type)             
-    train_list.each do |t|
-      if t.type == :cargo
-      @cargo_train += 1 
-      else @passenger_train += 1 
-      end
-    end
+    cargo_trains = trains.count { |t| t.type == :cargo }
+    passenger_trains = trains.count { |t| t.type == :passenger }
+    return cargo_trains, passenger_trains
   end
 
 end
--- ProjectRailway/class_train.rb
+++ ProjectRailway/class_train.rb
@@ -1,18 +1,20 @@
 class Train
   require_relative "class_route"
 
-  attr_accessor :hop, :current_route
+  attr_accessor :current_station_index, :current_route
 
   def initialize (number, type, qtty)
-    @number = number      #номер поезда
-    @type = type          #тип поезда
-    @qtty = qtty          #количество вагонов
-    @speed = 0            #поезд стоит
-    @hop = 0              #индекс текущей станции
+    @number = number                        #номер поезда
+    @type = type                            #тип поезда
+    @qtty = qtty                            #количество вагонов
+    @speed = 0                              #поезд стоит
+    @current_station_index = 0              #индекс текущей станции
   end
 
   def take_route(route)   #передаем объект класса Маршрут
     self.current_route = route 
+    self.current_station 
+    @current_station.train_in(self)
   end
 
   def speed_up
@@ -23,37 +25,38 @@ class Train
     @speed = 0
   end
 
-  def c_s                  #возвращает текущую станцию
-    @current_station = current_route.stations[hop]
+  def current_station                  #возвращает текущую станцию
+    current_route.stations[current_station_index]
   end
 
   def move_forward
-    self.c_s 
+    self.current_station 
     unless @current_station == current_route.stations[-1]
       @current_station.train_out(self)
       self.speed_up
       self.stop
-      self.hop += 1
+      current_station_index += 1
       @current_station.train_in(self)
     end
   end
 
   def move_back
-    self.c_s 
+    self.current_station
     unless @current_station == @current_route.stations[0]
       @current_station.train_out(self)
       self.speed_up
       self.stop
-      self.hop -= 1
+      current_station_index -= 1
       @current_station.train_in(self)
     end
   end
 
-  def report_nearest
-    self.c_s 
-    @previous_station = @current_route.stations[hop-1]
-    @next_station = @current_route.stations[hop+1]
-    return @previous_station, @current_station, @next_station
+  def report_previous
+    @previous_station = @current_route.stations[current_station_index-1]
+  end
+
+  def report_next
+    @next_station = @current_route.stations[current_station_index+1]
   end
 
 end
