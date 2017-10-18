80c4c064cc4fd9b1664c2ab65c30a5b7b55d40e6
v-kazantsev <vvs201@mail.ru>
2017-10-18 23:12:10 +0900

    Убрал self

--- ProjectRailway/class_train.rb
+++ ProjectRailway/class_train.rb
@@ -13,7 +13,7 @@ class Train
 
   def take_route(route)   #передаем объект класса Маршрут
     self.current_route = route  
-    self.current_station.train_in(self)
+    current_station.train_in(self)
   end
 
   def speed_up
@@ -25,35 +25,35 @@ class Train
   end
 
   def current_station                  #возвращает текущую станцию
-    current_route.stations[current_station_index]
+    current_route.stations[@current_station_index]
   end
 
   def move_forward
-    unless self.current_station == current_route.stations[-1]
-      self.current_station.train_out(self)
-      self.speed_up
-      self.stop
-      current_station_index += 1
-      self.current_station.train_in(self)
+    unless current_station == current_route.stations[-1]
+      current_station.train_out(self)
+      speed_up
+      stop
+      @current_station_index += 1
+      current_station.train_in(self)
     end
   end
 
   def move_back
-    unless self.current_station == current_route.stations[0]
-      self.current_station.train_out(self)
-      self.speed_up
-      self.stop
-      current_station_index -= 1
-      self.current_station.train_in(self)
+    unless current_station == current_route.stations[0]
+      current_station.train_out(self)
+      speed_up
+      stop
+      @current_station_index -= 1
+      current_station.train_in(self)
     end
   end
 
   def report_previous
-    @previous_station = current_route.stations[current_station_index-1]
+    current_route.stations[@current_station_index-1]
   end
 
   def report_next
-    @next_station = current_route.stations[current_station_index+1]
+    current_route.stations[@current_station_index+1]
   end
 
 end
