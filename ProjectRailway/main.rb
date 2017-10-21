296cd22da386b9a7fc2d098218d63ee3b2e1cd0e
v-kazantsev <vvs201@mail.ru>
2017-10-21 21:40:22 +0900

    Заменил на методы

--- ProjectRailway/main.rb
+++ ProjectRailway/main.rb
@@ -7,19 +7,133 @@ require_relative 'class_wagon'
 
 
 @all_stations = []
-def all_stations=(all_stations)
-  @all_stations = all_stations
-end
-def all_stations
-  @all_stations
+@all_trains = []
+
+def create_station
+  puts "ВВЕДИТЕ ИМЯ СТАНЦИИ"
+  station_name = gets.chomp.upcase
+  @station = Station.new(station_name)
+  @all_stations << @station 
+  puts "СТАНЦИЯ #{station_name} СОЗДАНА"
 end
 
-@all_trains = []
-def all_trains=(all_trains)
-  @all_trains = all_trains
+def create_train
+  puts "ВВЕДИТЕ НОМЕР ПОЕЗДА"
+  train_number = gets.chomp
+  puts "ВВЕДИТЕ ТИП ПОЕЗДА"
+  puts "[1] ГРУЗОВОЙ"
+  puts "[2] ПАССАЖИРСКИЙ"
+  train_type = gets.chomp
+  if train_type == "1" 
+    @train = CargoTrain.new(train_number)
+    @all_trains << @train 
+    @route_assigned = false
+    puts "СОЗДАН ГРУЗОВОЙ ПОЕЗД #{train_number}"
+  else
+    puts "СОЗДАН ПАССАЖИРСКИЙ ПОЕЗД #{train_number}"
+    @train = PassengerTrain.new(train_number)
+    @all_trains << @train 
+    @route_assigned = false
+  end
 end
-def all_trains
-  @all_trains
+
+def create_route
+  puts "ВВЕДИТЕ НАЧАЛЬНУЮ СТАНЦИЮ МАРШРУТА"
+  start = gets.chomp.upcase
+  from = @all_stations.detect { |x| x.name == start }
+  puts "ВВЕДИТЕ КОНЕЧНУЮ СТАНЦИЮ МАРШРУТА"
+  finish =  gets.chomp.upcase
+  to = @all_stations.detect { |x| x.name == finish }
+  @route = Route.new(from,to)
+  @route_created = true 
+  puts "МАРШРУТ #{start} - #{finish} СОЗДАН"
+end
+
+def add_station_to_route
+  if @route_created
+  puts "ВВЕДИТЕ ИМЯ СТАНЦИИ"
+  station_name = gets.chomp.upcase
+  find_station = @all_stations.detect { |x| x.name == station_name }
+  @route.add_station(find_station)
+  puts "СТАНЦИЯ #{station_name} ДОБАВЛЕНА"
+  else
+    puts "МАРШРУТ ЕЩЕ НЕ СОЗДАН"
+  end
+end
+
+def remove_station_from_route
+  puts "ВВЕДИТЕ ИМЯ СТАНЦИИ"
+  station_name = gets.chomp.upcase
+  find_station = @all_stations.detect { |x| x.name == station_name }
+  @route.remove_station(find_station)
+  puts "СТАНЦИЯ #{station_name} УБРАНА"
+end
+
+def assign_route
+  if @route_created
+  puts "ВВЕДИТЕ НОМЕР ПОЕЗДА"
+  train_number = gets.chomp
+  find_train = @all_trains.detect{ |x| x.number == train_number }
+  find_train.take_route(@route)
+  @route_assigned = true
+  puts "МАРШРУТ НАЗНАЧЕН"
+  else 
+    puts "МАРШРУТ ЕЩЕ НЕ СОЗДАН"
+  end
+end
+
+def add_wagon_by_type
+  puts "[1] ДОБАВИТЬ ГРУЗОВОЙ ВАГОН"
+  puts "[2] ДОБАВИТЬ ПАССАЖИРСКИЙ ВАГОН"
+  choice2 = gets.chomp
+  if choice2 == "1"
+  @wagon = Wagon.new(:cargo)
+  @train.add_wagon(@wagon)
+  puts "ГРУЗОВОЙ ВАГОН ДОБАВЛЕН"
+    elsif choice2 == "2"
+      @wagon = Wagon.new(:passenger)
+      @train.add_wagon(@wagon)
+  end  
+end
+
+def remove_wagon
+  @train.remove_wagon(@wagon)
+end
+
+def move_train_forward
+  puts "ВВЕДИТЕ НОМЕР ПОЕЗДА"
+  train_number = gets.chomp
+  find_train = @all_trains.detect{ |x| x.number == train_number }
+  find_train.move_forward
+end
+
+def move_train_back
+  puts "ВВЕДИТЕ НОМЕР ПОЕЗДА"
+  train_number = gets.chomp
+  find_train = @all_trains.detect{ |x| x.number == train_number }
+  find_train.move_back
+end
+
+def put_trains_info
+  puts "ВВЕДИТЕ НОМЕР ПОЕЗДА"
+  train_number = gets.chomp
+  find_train = @all_trains.detect{ |x| x.number == train_number }
+  unless @route_assigned
+  puts "СНАЧАЛА НАДО НАЗНАЧИТЬ МАРШРУТ"
+    else
+      if find_train.type == :cargo
+      current_train_type = "ГРУЗОВОЙ"
+        elsif find_train.type == :passenger 
+        current_train_type = "ПАССАЖИРСКИЙ"
+      end
+    puts current_train_type + " ПОЕЗД #{train_number} НАХОДИТСЯ НА СТАНЦИИ #{find_train.current_station.name}
+    И СОСТОИТ ИЗ #{find_train.wagons.size} ВАГОНОВ"
+  end
+end
+
+def put_stations_info
+  @all_stations.detect { |x| puts "#{x.name} ПАССАЖИРСКИХ ПОЕЗДОВ #{x.train_type_list(:passenger)}
+  ГРУЗОВЫХ ПОЕЗДОВ #{x.train_type_list(:cargo)}" }
 end
 
 choice = "0"
@@ -36,43 +150,17 @@ choice = gets.chomp
 case choice
   when "1"
 
-  puts "ВВЕДИТЕ ИМЯ СТАНЦИИ"
-  station_name = gets.chomp.upcase
-  station = Station.new(station_name)
-  all_stations << station 
-  puts "СТАНЦИЯ #{station_name} СОЗДАНА"
-
+  create_station
+  
   when "2" 
 
-  puts "ВВЕДИТЕ НОМЕР ПОЕЗДА"
-  train_number = gets.chomp
-  puts "ВВЕДИТЕ ТИП ПОЕЗДА"
-  puts "[1] ГРУЗОВОЙ"
-  puts "[2] ПАССАЖИРСКИЙ"
-  train_type = gets.chomp
-  if train_type == "1" 
-    train = CargoTrain.new(train_number)
-    all_trains << train 
-    puts "СОЗДАН ГРУЗОВОЙ ПОЕЗД #{train_number}"
-  else
-    puts "СОЗДАН ПАССАЖИРСКИЙ ПОЕЗД #{train_number}"
-    train = PassengerTrain.new(train_number)
-    all_trains << train 
-  end
+  create_train
 
   when "3" 
 
-  puts "ВВЕДИТЕ НАЧАЛЬНУЮ СТАНЦИЮ МАРШРУТА"
-  start = gets.chomp.upcase
-  from = all_stations.detect { |x| x.name == start }
-  puts "ВВЕДИТЕ КОНЕЧНУЮ СТАНЦИЮ МАРШРУТА"
-  finish =  gets.chomp.upcase
-  to = all_stations.detect { |x| x.name == finish }
-  route = Route.new(from,to)
-  puts "МАРШРУТ #{start} - #{finish} СОЗДАН"
+  create_route
 
   when "4"
-
 
     puts "[1] ДОБАВИТЬ СТАНЦИЮ В  МАРШРУТ"
     puts "[2] УБРАТЬ СТАНЦИЮ ИЗ МАРШРУТА"
@@ -88,69 +176,41 @@ case choice
 
     case choice1
       when "1"
-      puts "ВВЕДИТЕ ИМЯ СТАНЦИИ"
-      station_name = gets.chomp.upcase
-      find_station = all_stations.detect { |x| x.name == station_name }
-      route.add_station(find_station)
-      puts "СТАНЦИЯ #{station_name} ДОБАВЛЕНА"
+
+      add_station_to_route
 
       when "2"
-      puts "ВВЕДИТЕ ИМЯ СТАНЦИИ"
-      station_name = gets.chomp.upcase
-      find_station = all_stations.detect { |x| x.name == station_name }
-      route.remove_station(find_station)
-      puts "СТАНЦИЯ #{station_name} УБРАНА"
+      
+      remove_station_from_route
 
       when "3"
-      puts "ВВЕДИТЕ НОМЕР ПОЕЗДА"
-      train_number = gets.chomp
-      find_train = all_trains.detect{ |x| x.number == train.number }
-      find_train.take_route(route)
-      route_assigned = true
-      puts "МАРШРУТ НАЗНАЧЕН"
+
+      assign_route
       
       when "4"
-      puts "[1] ДОБАВИТЬ ГРУЗОВОЙ ВАГОН"
-      puts "[2] ДОБАВИТЬ ПАССАЖИРСКИЙ ВАГОН"
-      choice2 = gets.chomp
-      if choice2 == "1"
-        wagon = Wagon.new(:cargo)
-        train.add_wagon(wagon)
-        puts "ГРУЗОВОЙ ВАГОН ДОБАВЛЕН"
-      elsif choice2 == "2"
-        wagon = Wagon.new(:passenger)
-        train.add_wagon(wagon)
-      end  
+
+      add_wagon_by_type
+      
       when "5"
-      train.remove_wagon(wagon)
+      
+      remove_wagon
+
       when "6"
-      puts "ВВЕДИТЕ НОМЕР ПОЕЗДА"
-      train_number = gets.chomp
-      find_train = all_trains.detect{ |x| x.number == train.number }
-      find_train.move_forward
+
+      move_train_forward
+      
       when "7"
-      puts "ВВЕДИТЕ НОМЕР ПОЕЗДА"
-      train_number = gets.chomp
-      find_train = all_trains.detect{ |x| x.number == train.number }
-      find_train.move_back
+
+      move_train_back
+      
       when "8"
-      puts "ВВЕДИТЕ НОМЕР ПОЕЗДА"
-      train_number = gets.chomp
-      find_train = all_trains.detect{ |x| x.number == train.number }
-      unless route_assigned
-        puts "СНАЧАЛА НАДО НАЗНАЧИТЬ МАРШРУТ"
-      else
-      if find_train.type == :cargo
-        current_train_type = "ГРУЗОВОЙ"
-      elsif find_train.type == :passenger 
-        current_train_type = "ПАССАЖИРСКИЙ"
-      end
-      puts current_train_type + " ПОЕЗД #{train_number} НАХОДИТСЯ НА СТАНЦИИ #{find_train.current_station.name}
-      И СОСТОИТ ИЗ #{find_train.wagons.size} ВАГОНОВ"
-      end
+
+      put_trains_info
+      
       when "9"
-      @all_stations.detect { |x| puts "#{x.name} ПАССАЖИРСКИХ ПОЕЗДОВ #{x.train_type_list(:passenger)}
-      ГРУЗОВЫХ ПОЕЗДОВ #{x.train_type_list(:cargo)}" }
+
+      put_stations_info
+      
   end
 end
 end
