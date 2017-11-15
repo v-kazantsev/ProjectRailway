becf6deffe12da1360698beb71efa9b038815af4
v-kazantsev <vvs201@mail.ru>
2017-11-15 11:16:36 +0900

    Изменил валидаторы, исправил ошибки

--- ProjectRailway/Validation/module_validation.rb
+++ ProjectRailway/Validation/module_validation.rb
@@ -6,49 +6,43 @@ module Validation
   end
 
   module ClassMethods
-    attr_accessor :validate_macros
+    attr_accessor :validations
 
     def validate(name, *args)
-      @validate_macros ||= Hash.new { |hash, key| hash[key] = [] }
-      args.each { |value| @validate_macros[name] << value }
+      @validations ||= Hash.new { |hash, key| hash[key] = [] }
+      @validations[name]  << args 
     end
   end
 
   module InstanceMethods
 
-    def validate_presence
-      self.class.validate_macros.each do |key,value| 
-        if value[0] == :presence
-          raise RuntimeError, 'Value is nil' unless instance_variable_get("@#{key}")
-        end
-      end
-    end
-
-    def validate_type
-      self.class.validate_macros.each do |key,value|
-        if value[0] == :type
-          raise RuntimeError, 'Type mismatch' unless instance_variable_get("@#{key}").is_a? value[1]
-        end
-      end
-    end
-
-    def validate_format
-      self.class.validate_macros.each do |key,value|
-        if value[0] == :format
-          raise RuntimeError, 'Wrong format' unless instance_variable_get("@#{key}") =~ value[1]
-        end
-      end
-    end
-
-    def validate!
-      self.class.validate_macros.each { |i,j| self.send("validate_#{j[0]}".to_sym) }
-      return true
-    end
-
     def valid?
       validate!
       rescue RuntimeError
       false
+    end
+
+    private
+
+    def validate_presence(var,param)
+      raise RuntimeError, 'Value is nil' unless instance_variable_get("@#{var}")
+    end
+
+    def validate_type(var, param)
+      raise RuntimeError, 'Type mismatch' unless instance_variable_get("@#{var}").is_a? param
+    end
+
+    def validate_format(var,param)
+      raise RuntimeError, 'Wrong format' unless instance_variable_get("@#{var}") =~ param
+    end
+
+    def validate!
+      self.class.validations.each do |key, value|
+        value.each do |param|
+          self.send("validate_#{param[0]}", key, param[-1]) 
+        end
+      end
+      return true
     end
   end
 end
