16c15f83dccf04289c40285bc8aba420f2d5aeba
v-kazantsev <vvs201@mail.ru>
2017-11-15 11:18:05 +0900

    Добавил валидаторы атрибутам

--- ProjectRailway/Validation/validation_test.rb
+++ ProjectRailway/Validation/validation_test.rb
@@ -1,9 +1,11 @@
 require_relative 'class_station'
+require_relative 'class_train'
 require_relative 'module_validation'
 class Test
   include Validation
   attr_accessor :name, :station
   validate :name, :presence
+  validate :name, :type, String
   validate :name, :format, /\A\w+/i
   validate :station, :type, Station
 end
@@ -11,6 +13,7 @@ end
 class Test2
   include Validation
   attr_accessor :number, :train
+  validate :number, :presence
   validate :number, :format, /\A[a-zа-я0-9]{3}-*[a-zа-я0-9]{2}\z/i
   validate :train, :type, Train
 end
